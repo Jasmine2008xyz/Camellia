096ed1f9e744e278bea6468286e3b68bd515db71
zhidong813 <2968447202@qq.com>
2024-09-08 13:45:42 +0800

    f

--- app/src/main/cpp/camellia.cpp
+++ app/src/main/cpp/camellia.cpp
@@ -8,7 +8,7 @@ extern "C" {
 
 void Java_com_luoyu_camellia_interfaces_jni_NativeEntry_init(JNIEnv* env, jobject thiz)
 {
-    jclass cls = env->FindClass(env,"com/luoyu/camellia/utils/FileUtil");
+  //  jclass cls = env->FindClass("com/luoyu/camellia/utils/FileUtil");
     //jmethodID mid = env->GetMethodID(cls, "writeToFile", "(Ljava/lang/String;Ljava/lang/String;)V");
    // jstring path = env->NewStringUTF("/storage/emulated/0/Android/media/com.luoyu.camellia/吴迪.txt");
   //  jstring content = env->NewStringUTF("吴迪");
