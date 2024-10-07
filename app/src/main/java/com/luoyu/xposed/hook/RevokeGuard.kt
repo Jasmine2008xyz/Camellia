45d09893e0425f2020425c733f618a7701dbba59
Xiao Ming <158139013+Jasmine2008xyz@users.noreply.github.com>
2024-10-07 02:48:26 +0000

    测试

---
+++ app/src/main/java/com/luoyu/xposed/hook/RevokeGuard.kt
@@ -0,0 +1,19 @@
+package com.luoyu.xposed.hook
+
+import com.luoyu.xposed.base.annotations.Xposed_Item_Controller
+import com.luoyu.xposed.base.annotations.Xposed_Item_Entry
+import com.luoyu.xposed.base.annotations.Xposed_Item_Finder
+
+@Xposed_Item_Controller
+class RevokeGuard  {
+
+/*@Xposed_Item_Finder
+    fun find(finder: IDexFinder) {
+        
+    }*/
+
+    @Xposed_Item_Entry
+    fun init() {
+        
+    }
+}
---
+++ app/src/main/java/com/luoyu/xposed/hook/api/QQNTOnMessage.kt
@@ -0,0 +1,13 @@
+package com.luoyu.xposed.hook.api
+
+import com.luoyu.xposed.base.annotations.Xposed_Item_Controller
+import com.luoyu.xposed.base.annotations.Xposed_Item_Entry
+import com.luoyu.xposed.base.annotations.Xposed_Item_Finder
+import com.luoyu.dexfinder.IDexFinder
+@Xposed_Item_Controller(itemTag = "QQNTOnMessage(QQNT消息监听)", isApi = true)
+class QQNTOnMessage {
+    @Xposed_Item_Finder
+    fun findMethod(finder: IDexFinder) {
+        finder.findMethodsByPathAndUseString(arrayOf("AutoOpenRedPacket_1"),arrayOf("com.tencent.qqnt.msg"),arrayOf("[不支持的元素类型]", "[图片]", "[文件]", "[emoji]"));
+    }
+}
