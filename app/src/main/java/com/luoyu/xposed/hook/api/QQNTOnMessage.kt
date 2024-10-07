d4fdbeb22f40773682adbe991b83e0d8bccfcfa5
Xiao Ming <158139013+Jasmine2008xyz@users.noreply.github.com>
2024-10-07 14:25:43 +0000

    callback

--- app/src/main/java/com/luoyu/xposed/hook/api/QQNTOnMessage.kt
+++ app/src/main/java/com/luoyu/xposed/hook/api/QQNTOnMessage.kt
@@ -3,11 +3,28 @@ package com.luoyu.xposed.hook.api
 import com.luoyu.xposed.base.annotations.Xposed_Item_Controller
 import com.luoyu.xposed.base.annotations.Xposed_Item_Entry
 import com.luoyu.xposed.base.annotations.Xposed_Item_Finder
+import com.luoyu.xposed.core.HookInstaller
 import com.luoyu.dexfinder.IDexFinder
+
+import de.robv.android.xposed.XposedHelpers
+import de.robv.android.xposed.XposedBridge
+import de.robv.android.xposed.callbacks.XC_MethodHook
+import java.lang.reflect.Method
+
 @Xposed_Item_Controller(itemTag = "QQNTOnMessage(QQNT消息监听)", isApi = true)
 class QQNTOnMessage {
     @Xposed_Item_Finder
     fun findMethod(finder: IDexFinder) {
-        finder.findMethodsByPathAndUseString(arrayOf("AutoOpenRedPacket_1"),arrayOf("com.tencent.qqnt.msg"),arrayOf("[不支持的元素类型]", "[图片]", "[文件]", "[emoji]"))
+        finder.findMethodsByPathAndUseString(arrayOf("QQNTOnMessage_1"),arrayOf("com.tencent.qqnt.msg"),arrayOf("[不支持的元素类型]", "[图片]", "[文件]", "[emoji]"))
+    }
+    @Xposed_Item_Entry
+    fun entry() {
+        val m: Method = HookInstaller.Method_Map.get("QQNTOnMessage_1")
+        val method = XposedHelpers.findMethodsByExactParameters(m.declaringClass, m.returnType, m.parameterTypes)[0]
+        XposedBridge.hookMethod(method, object : XC_MethodHook() {
+            override fun afterHookedMethod(param: MethodHookParam) {
+                
+            }
+        })
     }
 }
