35876b2c233712fb132c0413da341c92da1b84da
Jasmine2008xyz <2968447202@qq.com>
2024-11-27 22:50:10 +0800

    b

---
+++ app/src/main/java/com/luoyu/internal/theme/HookTheme.java
@@ -0,0 +1,4 @@
+package com.luoyu.internal.theme;
+
+public class HookTheme {
+}
--- app/src/main/java/com/luoyu/xposed/base/Constants.java
+++ app/src/main/java/com/luoyu/xposed/base/Constants.java
@@ -1,4 +1,5 @@
 package com.luoyu.xposed.base;
 
 public class Constants {
+  public static final String QQ_PACKAGE_NAME = "com.tencent.mobileqq";
 }
