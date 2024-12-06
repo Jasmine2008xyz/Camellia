f30f064f3f3f4217ffe616d0edc98a0bf4ad17f0
Jasmine2008xyz <2968447202@qq.com>
2024-12-06 13:24:26 +0800

    Update README.md

--- README.md
+++ README.md
@@ -8,14 +8,21 @@ Camellia 是一个开源的 Xposed 模块，旨在对QQ进行功能扩展、为 
 
 - 获取最新版本： [![GitHub release](https://img.shields.io/github/release/Jasmine2008xyz/Camellia.svg)](https://github.com/Jasmine2008xyz/Camellia/releases/latest)
 - 请确保您会使用 **Xposed 框架**来装载本模块
-- 如果您使用的是 **Lsposed** ，请勾选模块作用域为 **QQ**
+- 如果您使用的是 **[Lsposed](https://github.com/LSPosed/LSPosed)** ，请勾选模块作用域为 **QQ**
+- **[注意] 本模块的支持版本范围为9.0~9.1.23+(最新版)**
 
 ## 额外说明
 
 模块尚在开发中，因此出现一系列**未知 bug 及不可控情况**是正常的情况， 我们也更推荐您使用最新版本来避免此类情况的发生
-也欢迎您在 issue  提出功能开发建议以及 bug 反馈
+也欢迎您在 issue  提出功能开发建议以及 bug 反馈。
+
+本模块仅供学习参考，请勿将本模块用于非法用途。
 
 给我们点点Star，不要逼我求你。
+
+## 编译
+- JDK>=17.0.9
+- NDK ... 你看我们用了吗？(
 
 ------------
 
@@ -34,9 +41,75 @@ Camellia 是一个开源的 Xposed 模块，旨在对QQ进行功能扩展、为 
 - 跳转网页功能
 - 解锁输入框限制
 - 屏蔽QQ通知
+- ......
 
 ### 即将支持的功能
 - ~~随机开发者女装照~~
 - 长按发送按钮发送卡片消息
 - 百变气泡
 
+
+### 我们的优势
+
+#### 真·清晰易懂的代码逻辑
+没有使用复杂的依赖库，使得我们的代码逻辑清晰易懂易移植。
+因为太复杂的库我们两个看不懂不会用。
+
+#### 看似规范且条理清晰的注解配上逆天的代码
+
+以下逆天代码与注解曾真实存在于项目之中：
+```java
+      // 开始添加Item
+  root.addView(
+          Getbutton(
+                  "Java插件",
+                  v -> {
+                      /** 这里因为直接升起不了Activity，所以为了偷懒，先退出当前活动 **/
+                      /**
+                       * 不要紧的，我会在JavaPluginActivity里重写退出按钮，他们就只有返回键可以按，退出后自动升回来，反正速度很快他们看不出来
+                       */
+                       onBackPressed();                         
+                       startActivity(
+                              new Intent(SettingActivity.this, JavaPluginActivity.class));
+                  }));
+``` 
+其实大部分地方是根本没有注解的(
+
+#### 简短的变量名压缩源码大小
+以下代码摘自公式的防撤回项目(现在没了)
+```java
+public static void Seach(ArrayList a) {
+    try {
+
+      Object q = a.get(0);
+            
+      String b = FieldUtils.GetField(q, "msgId").toString();
+      String g = FieldUtils.GetField(q, "senderUin").toString();
+      String ll = FieldUtils.GetField(q, "sendNickName").toString();
+      MsgRecordTable.senduin.put(b, g);
+      MsgRecordTable.sendnick.put(b, ll);
+      //MsgRecordTable.msg.put(b,q);
+      ArrayList p = FieldUtils.GetField(q, "elements");
+      Object m = p.get(0);
+      Integer elementtype = FieldUtils.GetField(m, "elementType");
+      if (elementtype == 1) {
+        Object textele = FieldUtils.GetField(m, "textElement");
+
+        String mm = FieldUtils.GetField(textele, "content");
+        MsgRecordTable.msglist.put(b, mm);
+      }
+
+    } catch (Exception e) {
+      ForLogUtils.Debug("Search", e);
+    }
+  }
+```
+
+### 2024/12/06 公告
+现行的Camellia是**臃肿的，笨重的**。其程度远高于旧项目"公式"。
+所以我打算采用新架构重构本项目核心并标准化项目。
+下一个正式版本将为1.0.0，在重构期间的过渡版本接续目前的版本号0.2.7.**
+
+
+### 支持我们
+![](http://103.24.204.23/lzj/赞助码.jpg)
--- app/src/main/java/com/luoyu/xposed/hook/crash/PreventCrash_Entry.java
+++ app/src/main/java/com/luoyu/xposed/hook/crash/PreventCrash_Entry.java
@@ -9,52 +9,72 @@ import com.google.android.material.dialog.MaterialAlertDialogBuilder;
 import com.luoyu.camellia.activities.helper.ActivityAttributes;
 import com.luoyu.utils.ParseUtil;
 import com.luoyu.xposed.ModuleController;
+import com.luoyu.xposed.base.HookEnv;
 import com.luoyu.xposed.base.annotations.Xposed_Item_Controller;
 import com.luoyu.xposed.base.annotations.Xposed_Item_UiLongClick;
 import de.robv.android.xposed.XC_MethodHook;
 import de.robv.android.xposed.XposedBridge;
+import de.robv.android.xposed.XposedHelpers;
 
 @Xposed_Item_Controller(isApi = false, itemTag = "阻止Java层闪退")
 public class PreventCrash_Entry {
-    @Xposed_Item_UiLongClick
-    public void onLongClick() {
-        MaterialAlertDialogBuilder dialog =
-                new MaterialAlertDialogBuilder(ActivityAttributes.context);
-        TextView tv = new TextView(ActivityAttributes.context);
-        LinearLayout root = new LinearLayout(ActivityAttributes.context);
-        root.setGravity(Gravity.CENTER);
-        Switch switzh = new Switch(ActivityAttributes.context);
-        tv.setText("是否弹窗/打印日志");
-        tv.setTextSize(20);
-        tv.setTextColor(ParseUtil.parseColor("#1A1A1A"));
-        root.setOrientation(LinearLayout.HORIZONTAL);
-        switzh.setChecked(ModuleController.Config.getBooleanData("阻止Java层闪退/打印日志", false));
+  @Xposed_Item_UiLongClick
+  public void onLongClick() {
+    MaterialAlertDialogBuilder dialog = new MaterialAlertDialogBuilder(ActivityAttributes.context);
+    TextView tv = new TextView(ActivityAttributes.context);
+    LinearLayout root = new LinearLayout(ActivityAttributes.context);
+    root.setGravity(Gravity.CENTER);
+    Switch switzh = new Switch(ActivityAttributes.context);
+    tv.setText("是否弹窗/打印日志");
+    tv.setTextSize(20);
+    tv.setTextColor(ParseUtil.parseColor("#1A1A1A"));
+    root.setOrientation(LinearLayout.HORIZONTAL);
+    switzh.setChecked(ModuleController.Config.getBooleanData("阻止Java层闪退/打印日志", false));
 
-        switzh.setOnCheckedChangeListener(
-                (ch, boo) -> {
-                    ModuleController.Config.putData("阻止Java层闪退/打印日志", boo);
-                    Toast.makeText(ActivityAttributes.context, "设置成功！", Toast.LENGTH_LONG).show();
-                });
-        root.addView(tv);
-        root.addView(switzh);
-        dialog.setView(root);
-        dialog.setTitle("阻止Java层闪退");
-        dialog.setPositiveButton("Leave", null);
-        dialog.show();
-       try {
-    	
-   
+    switzh.setOnCheckedChangeListener(
+        (ch, boo) -> {
+          ModuleController.Config.putData("阻止Java层闪退/打印日志", boo);
+          Toast.makeText(ActivityAttributes.context, "设置成功！", Toast.LENGTH_LONG).show();
+        });
+    root.addView(tv);
+    root.addView(switzh);
+    dialog.setView(root);
+    dialog.setTitle("阻止Java层闪退");
+    dialog.setPositiveButton("Leave", null);
+    dialog.show();
+
+    /*
     XposedBridge.hookMethod(Class.class.getDeclaredMethod("forName",String.class),new XC_MethodHook(){
       @Override
         protected void afterHookedMethod(MethodHookParam param) {
-            if(param.getResult()!=null){
-        	if(((Class)param.getResult()).getName().startsWith("com.tencent.qqnt")) {
-        		XposedBridge.log("Loaded class: " + param.getResult());
+            if(param.args[0]!=null){
+        	if(param.args[0].toString().startsWith("com.tencent.qqnt")) {
+        		XposedBridge.log("Loaded class: " + param.args[0]);
         	}
         }}
-    });
- } catch(Exception err) {
-    	
+    });*/
+
+    try {
+
+      // Hook ClassLoader的loadClass方法
+      XposedBridge.hookMethod(
+          Class.class.getMethod("forName",String.class),
+          new XC_MethodHook() {
+            @Override
+            protected void beforeHookedMethod(MethodHookParam param) throws Throwable {
+              // 打印加载的类名
+              String className = (String) param.thisObject.toString();
+              XposedBridge.log("Loading class: " + className);
+            }
+
+            @Override
+            protected void afterHookedMethod(MethodHookParam param) throws Throwable {
+              // 可以在这里做一些后处理操作
+            }
+          });
+      HookEnv.getHostClassLoader().loadClass("java.lang.Thread");
+    } catch (Exception e) {
+      XposedBridge.log("Error hooking ClassLoader: " + e.getMessage());
     }
-    }
+  }
 }
